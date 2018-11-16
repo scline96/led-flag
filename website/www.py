0257a393f087bb914c1e2ddd4d7edbbc61ba9e2c
Seth Penna <spenna@lutron.com>
2018-11-16 08:27:10 -0500

    Added comments

--- website/www.py
+++ website/www.py
@@ -1,16 +1,32 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
+
+## Setup the flask application and socketio object
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+
+## When the user navigates to the '/' base URL
 @app.route('/')
 def home():
-        return render_template('index.html')
+    # Return the index page
+    return render_template('index.html')
 
-
+## On the socketio call 'button'
 @socketio.on('button', namespace='/led')
 def buttonPressed(button_name):
+    # Print that the user pressed a button
     print(str(button_name) + ' pressed!')
 
-socketio.run(app, use_reloader=False, port=5000, host='0.0.0.0', debug=True)
+    '''
+        This is where the LED stuff can happen, we can also add more
+        socketio.on statements for different buttons or other actions 
+        from the front-end.
+
+        We can also send data to javascript using the socketio.emit()
+    '''
+
+
+if __name__ == '__main__':
+    socketio.run(app, use_reloader=False, port=5000, host='0.0.0.0', debug=True)
