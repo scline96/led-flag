cc2d37fd3a83504d16e63b1b2b387e391b5f06da
Sarah Cline <cline.s@husky.neu.edu>
2018-11-29 22:46:04 +0000

    Text on website to matrix

--- website/static/button.js
+++ website/static/button.js
@@ -2,6 +2,7 @@ var socket = io.connect('http://' + document.domain + ":" + location.port);
 
 
 function button(){
-	socket.emit('button', 'name_of_button')
+	socket.emit('button',  document.getElementById('inputtext').value)
+	console.log( document.getElementById('inputtext').value)
 	console.log('button pressed')
 }
